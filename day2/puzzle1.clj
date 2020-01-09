e068feea8550711016cf9399057fcc8ffd14ce31
Michael Terrell <michael.terrell@britecore.com>
2020-01-09 12:01:24 -0600

    updating code to use atoms instead of strings

--- day2/puzzle1.clj
+++ day2/puzzle1.clj
@@ -8,15 +8,15 @@
 
 (defn execute-op [op ops idx]
   (cond
-    (= op "add") (reduce + (get-operands ops idx))
-    (= op "mul") (reduce * (get-operands ops idx))))
+    (= op :add) (reduce + (get-operands ops idx))
+    (= op :mul) (reduce * (get-operands ops idx))))
 
 (defn opcode-parser [ops idx]
   (let [x (get ops idx)]
     (cond
       (= x 99) ops
-      (= x 1) (recur (assoc ops (get ops (+ idx 3)) (execute-op "add" ops idx)) (+ idx 4))
-      (= x 2) (recur (assoc ops (get ops (+ idx 3)) (execute-op "mul" ops idx)) (+ idx 4))
+      (= x 1) (recur (assoc ops (get ops (+ idx 3)) (execute-op :add ops idx)) (+ idx 4))
+      (= x 2) (recur (assoc ops (get ops (+ idx 3)) (execute-op :mul ops idx)) (+ idx 4))
       :else (recur ops (+ idx 1)))))
 
 (defn compute [filename]
